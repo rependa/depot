b7ad0eda501fad023c6596781b4044f06186d0e4
Matt Rependa <matt.rependa@rci.rogers.com>
2014-10-17 11:28:36 -0400

    test fixtures - B3 playtime

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: {minimum: 10}
+  validates_length_of :title, minimum: 10, message: "title must be 10 characters or more"
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
